9a4357ef3a278552418e8f907d99da47002f872b
Luan Nguyen <luan.ndd.60cntt@ntu.edu.vn>
2023-03-22 16:43:34 +0700

    no message

--- src/Test.vue
+++ src/Test.vue
@@ -1,24 +1,30 @@
 <template>
   <div>
-    <div class="section-navbar">
+    <!-- <div class="section-navbar">
       <div class="navbar-wrapper">
         <Navbar />
       </div>
 
-      <!-- khúc này hiển thị nội dung được định tuyến, tức là cái ui lúc nãy mình import ở trang home á  -->
+      khúc này hiển thị nội dung được định tuyến, tức là cái ui lúc nãy mình import ở trang home á 
     </div>
-    <router-view />
+    <router-view /> -->
+    <div class="section-sidebar">
+      <div class="container">
+        <Sidebar />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/navbar.vue";
+import Sidebar from "./components/SideBar.vue";
 
 export default {
   name: "App",
   data() {
     return {};
   },
-  components: { Navbar },
+  components: { Navbar, Sidebar },
 };
 </script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <div class="sub-sidebar">
+    <div class="top-content">
+      <div class="left-info">
+        <a class="sidebar-logo">
+          <img src="../assets/img/pachama.png" />
+        </a>
+      </div>
+      <div class="right-info">
+        <div class="user-info">
+          <h3>Hello Admin,</h3>
+          <div class="user-avatar">
+            <img src="../assets/img/fujiMout.jpg" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-content">
+      <div class="left-sidebar">
+        <ul>
+          <li><router-link to="/">Management</router-link></li>
+          <li><router-link to="/about">Dash Board</router-link></li>
+          <li><router-link to="/Explore">Calender Management</router-link></li>
+        </ul>
+      </div>
+      <div class="right-content">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("../assets/css/sidebar.css");
+</style>
