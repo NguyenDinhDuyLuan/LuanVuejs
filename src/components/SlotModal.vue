cb3aa9e6f606430847e134cb4e5a5c5d8e3697f0
ThuyDesigner <ngthithuthuy96@gmail.com>
2023-03-28 16:25:19 +0700

    Comment "modal+slot"

--- src/components/SlotModal.vue
+++ src/components/SlotModal.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-xl @click="modalCard = !modalCard">
+    <!-- <b-button v-b-modal.modal-xl @click="modalCard = !modalCard">
     <slot></slot>
-    </b-button>
+    </b-button> -->
+    <!--  khúc naỳ nằm ngoài nha  -->
 
     <b-modal v-model="modalCard" id="modal-xl" size="xl">
       <SlotExample>
         <template #name>
           <h1>Card Title</h1>
+              <!-- data sẽ đưọc bên ngoài tuỳ chỉnh  -->
         </template>
 
         <template #detail>
-          <p>
+          <!-- <p>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer
-          </p>
+          </p> -->
+          <!-- data sẽ đưọc bên ngoài tuỳ chỉnh  -->
         </template>
 
         <template #status>
+              <!-- data sẽ đưọc bên ngoài tuỳ chỉnh  -->
           <p>Last updated 3 mins ago</p>
         </template>
       </SlotExample>
@@ -26,7 +30,7 @@
 </template>
 
 <script>
-import SlotExample from "../components/Slot.vue";
+import SlotExample from "../components/Slot.vue"; // khúc này chị k hiểu lắm 
 export default {
   data() {
     return { modalCard: false };
--- src/view/SlotModalPage.vue
+++ src/view/SlotModalPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="outer">
     <div>
-      <SlotModal> Modal Open</SlotModal>
+      <b-button v-b-modal.modal-xl @click="modalCard = !modalCard">
+        show model
+        <!--  gọi sự kiện để gọi modal show data lên  -->
+      </b-button>
+      <!--  khúc naỳ nằm ở đây  -->
+      <SlotModal>
+        <!--  đổ data vào đây theo slot nha em -->
+        <!--   -->
+      </SlotModal>
     </div>
   </div>
 </template>
